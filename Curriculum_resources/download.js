608b4dad3e5fa02edfaad49f7b19d7b89ec67572
Gilberto Shimokawa Falcao <contato.gilberto.shk.falcao@gmail.com>
2022-05-22 13:57:49 -0300

    Atualizacoes huperlinks

--- Curriculum_resources/download.js
+++
@@ -1,7 +0,0 @@
-function generatePDF() {
-  var printContents = document.getElementById('Pagina').innerHTML;
-  var originalContents = document.body.innerHTML;
-  document.body.innerHTML = printContents;
-  window.print();
-  document.body.innerHTML = originalContents;
-}
