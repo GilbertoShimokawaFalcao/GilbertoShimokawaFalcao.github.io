08f14da9e60ca0565876621d27ec9a765ace502d
GilbertoShimokawaFalcao <contato.gilberto.shk.falcao@gmail.com>
2022-06-06 16:39:55 -0300

    Atualizacoes Projetos

--- Curriculum/Recursos/download.js
+++ Curriculum/Recursos/download.js
@@ -1,10 +1,9 @@
-function generatePDF() {
+function generatePDF() {  
   var printContents = document.getElementById('Pagina').innerHTML;
   var originalContents = document.body.innerHTML;
   document.body.innerHTML = printContents;
   document.title = 'GilbertoShimokawaFalcaoCurriculumAnalistaSistemas_' + formatDate();
-  document
-  window.print();
+  window.print();  
   document.body.innerHTML = originalContents;
 }
 
@@ -14,10 +13,8 @@ function formatDate() {
       day = d.getDate(),
       year = d.getFullYear();
 
-  if (month.length < 2) 
-      month = '0' + month;
-  if (day.length < 2) 
-      day = '0' + day;
+  if (month.length == 1) month = '0' + month;
+  if (day.length == 1) day = '0' + day;
 
   return [year.toString() + month.toString() + day.toString()];
 }
