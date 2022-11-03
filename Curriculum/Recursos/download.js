07d748b43ad9982a285989ee11bb53f50853fcf7
GilbertoShimokawaFalcao <contato.gilberto.shk.falcao@gmail.com>
2022-11-02 23:04:02 -0300

    Ajustes

--- Curriculum/Recursos/download.js
+++ Curriculum/Recursos/download.js
@@ -2,22 +2,19 @@ function generatePDF() {
   var printContents = document.getElementById('imprimir').innerHTML;
   var originalContents = document.body.innerHTML;
   document.body.innerHTML = printContents;  
-  document.title = 'GilbertoShimokawaFalcaoCurriculumAnalistaSistemas_' + formatDate();
-  window.print();    
+  document.title = 'GilbertoShimokawaFalcaoCurriculumAnalistaSistemas_' + formatDate();  
+  window.print();      
   document.body.innerHTML = originalContents;
 }
 
 function formatDate() {
   var d = new Date(),
-      month = (d.getMonth() + 1),
+      month = d.getMonth() + 1,
       day = d.getDate(),
-      year = d.getFullYear();
-    
-  let monthCon = '';
-  let dayCon = '';
+      year = d.getFullYear();  
 
-  if (month.length == 1) monthCon = '0' + month;
-  if (day.length == 1) dayCon = '0' + day;
+  let monthString = month.length == 1 ? month.toString(): '0' + month.toString(); 
+  let dayString = day.length == 1 ? day.toString(): '0' + day.toString();
 
-  return [year.toString() + monthCon.toString() + dayCon.toString()];
+  return [year.toString() + monthString + dayString];
 }
