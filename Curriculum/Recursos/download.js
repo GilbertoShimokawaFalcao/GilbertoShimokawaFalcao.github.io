d4843cffed5d2f0361e5497dfde5806a063f7dfe
Gilberto <contato.gilberto.shk.falcao@gmail.com>
2022-12-25 02:58:38 -0300

    Curriculum

---
+++ Curriculum/Recursos/download.js
@@ -0,0 +1,20 @@
+function generatePDF() {  
+  var printContents = document.getElementById('imprimir').innerHTML;
+  var originalContents = document.body.innerHTML;
+  document.body.innerHTML = printContents;          
+  document.title = 'GilbertoShimokawaFalcaoCurriculumAnalistaSistemas_' + formatDate();  
+  window.print();        
+  document.body.innerHTML = originalContents;    
+}
+
+function formatDate() {
+  var d = new Date(),
+      month = d.getMonth() + 1,
+      day = d.getDate(),
+      year = d.getFullYear();
+
+      let monthString = month.toString().length == 1 ? monthString = '0' + month.toString() :  month.toString();    
+      let dayString = day.toString().length == 1 ? '0' + day.toString() : day.toString();
+  
+    return [year.toString() + monthString.toString() + dayString.toString()].toString();
+}
