e6856994a3644533bcf94be428e2fc2064c6e746
Gilberto <contato.gilberto.shk.falcao@gmail.com>
2023-01-16 13:29:37 -0300

    ajustes

--- Curriculum/Recursos/download.js
+++ Curriculum/Recursos/download.js
@@ -1,20 +1,22 @@
 function generatePDF() {  
+  let local = 'GilbertoShimokawaFalcaoCurriculumAnalistaSistemas_' + formatDate();
+  alert('Para melhor experiência, Salve o arquivo com:\nLayout: Colorido\nMargens: Nenhuma\Graficos de segundo plano.\nSugestão de nome documento: ' + local);
   var printContents = document.getElementById('imprimir').innerHTML;
   var originalContents = document.body.innerHTML;
-  document.body.innerHTML = printContents;          
-  document.title = 'GilbertoShimokawaFalcaoCurriculumAnalistaSistemas_' + formatDate();  
-  window.print();        
-  document.body.innerHTML = originalContents;    
+  document.body.innerHTML = printContents;       
+  document.title = local;
+  window.print();
+  document.body.innerHTML = originalContents;      
 }
 
-function formatDate() {
-  var d = new Date(),
-      month = d.getMonth() + 1,
-      day = d.getDate(),
+function formatDate() {    
+      var d = new Date();
+      month = d.getMonth() + 1;
+      day = d.getDate();
       year = d.getFullYear();
 
-      let monthString = month.toString().length == 1 ? monthString = '0' + month.toString() :  month.toString();    
+      let monthString = '';
+      month.toString().length == 1 ? monthString = '0' + month.toString() :  month.toString();    
       let dayString = day.toString().length == 1 ? '0' + day.toString() : day.toString();
-  
     return [year.toString() + monthString.toString() + dayString.toString()].toString();
 }
